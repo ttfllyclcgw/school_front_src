a0e104337cce3712083351f724fd7ecd9752fe42
曹灵聪 <527891214@qq.com>
2022-06-13 16:08:58 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,108 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <el-container class="layout-container-demo">
+    <el-aside width="200px">
+      <el-scrollbar>
+        <el-menu :default-openeds="['1', '3']">
+          <el-menu-item index="1">
+            <template #title>
+              <el-icon><House /></el-icon>首页
+            </template>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <template #title>
+              <el-icon><ChatDotRound /></el-icon>公告
+            </template>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <template #title>
+              <el-icon><User /></el-icon>教师管理
+            </template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <template #title>
+              <el-icon><User /></el-icon>学生管理
+            </template>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <template #title>
+              <el-icon><Document /></el-icon>图书管理
+            </template>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <template #title>
+              <el-icon><Monitor /></el-icon>操作历史
+            </template>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <template #title>
+              <el-icon><User /></el-icon>用户管理
+            </template>
+          </el-menu-item>
+          <el-menu-item index="8">
+            <template #title>
+              <el-icon><User /></el-icon>角色管理
+            </template>
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <div class="toolbar">
+          <el-dropdown>
+            <el-icon style="margin-right: 8px; margin-top: 1px"
+              ><setting
+            /></el-icon>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>View</el-dropdown-item>
+                <el-dropdown-item>Add</el-dropdown-item>
+                <el-dropdown-item>Delete</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          <span>Tom</span>
+        </div>
+      </el-header>
+
+      <el-main>
+        <el-scrollbar>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="Date" width="140" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="address" label="Address" />
+          </el-table>
+        </el-scrollbar>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+</script>
 
-nav {
-  padding: 30px;
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
 }
 </style>
